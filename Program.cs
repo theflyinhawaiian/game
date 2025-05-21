f06a2ccf3587d3a6104e5f33bd1bb10cb836c1fc
Peter Mullins <jc310xc@gmail.com>
2025-05-21 00:36:11 -0400

    loop homework for Vera

--- Program.cs
+++ Program.cs
@@ -3,9 +3,44 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter a number:");
-        var input = Console.ReadLine();
+        int[] numbers = new int[]
+        {
+            107, -67, 184, 58, -130, -174, -177, 105, -45, -125,
+            -16, -160, -71, 90, -63, -103, 37, -180, -33, -92,
+            119, -59, 13, -10, -15, 128, -132, 82, -91, 95,
+            -43, 30, -142, 108, 24, 1, -36, 19, 103, -94,
+            88, -2, 191, -196, -78, -149, -93, -134, 121, 177,
+            65, -193, 116, -140, -191, -143, -22, 132, -76, -56,
+            -197, 85, 112, 111, 98, -27, -102, -81, 71, -40,
+            31, -87, 129, -11, 28, 35, -129, 32, -164, 164,
+            147, 45, -53, 104, -65, 91, 42, -113, -192, -79,
+            -3, -148, -124, 134, 84, 178, 11, -83, 123, 182, -12,
+            -20, -151, 44, -21, 188, -49, 122, -115, -31, -41,
+            -14, -106, -176, -52, 72, -141, -24, 34, -105, -170,
+            -97, 125, -1, 11, -145, 159, -133, 99, -186, 156, -144,
+            -120, -154, 140, -154, 149, 14, 56, 162, 81, -126,
+            -139, 153, 11, -60, -122, 59, 27, 185, -179, 145, -150,
+            89, 36, 96, -183, -29, 9, -32, 154, -7, 146,
+            -6, 161, 124, 55, -4, -28, -111, 67, -107, -157,
+            -88, 135, 194, -116, -80, 0, 198, -13, 3, -38,
+            106, 157, -186, 20, -101, -90, -128, 143, 174, -8
+            };
 
-        Console.WriteLine($"Your number is: {input}");
+        /*
+            Your task is to solve the following problems using the array of integers above.
+            Use separate loops for each problem, and make sure to print the results to the console.
+
+            Problem #1: Find the average of the even-indexed numbers in the array.
+            Example: { -4, 13, 2, 10, 11}
+            Answer: (-4 + 2 + 11) / 3 = 3
+        
+            Problem #2: Determine if the majority of the numbers in the array are positive.
+            Example: { -4, 13, 2, 10, 11}
+            Answer: 4 positive numbers out of 5 total numbers, so majority is positive.
+
+            Problem #3: Find the index of the first occurring 11 in the array.
+            Example: { -4, 13, 2, 10, 11}
+            Answer: 4
+        */
     }
 }
