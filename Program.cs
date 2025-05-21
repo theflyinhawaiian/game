89ec64a8af03ef8e6cc172e065c0e9f4ad9b2c5a
Puffal <fireandfinn@gmail.com>
2025-05-20 22:01:33 -0400

    Made number guessing game

--- Program.cs
+++ Program.cs
@@ -1,11 +1,57 @@
-﻿
+﻿using System;
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter a number:");
-        var input = Console.ReadLine();
+        bool playAgain = true;
 
-        Console.WriteLine($"Your number is: {input}");
+        while (playAgain == true)
+        {
+            Console.WriteLine("Guess a number between 1-20:");
+            var input = Console.ReadLine();
+            int intTemp = Convert.ToInt32(input);
+
+
+            var random = new Random();
+            var ranNum = random.Next(20);
+            ranNum = ranNum + 1;
+
+            int guessCounter = 1;
+
+            while (intTemp != ranNum)
+            {
+                if (intTemp >= ranNum)
+                {
+                    Console.WriteLine("the number im thinking of is lower");
+                }
+                if (intTemp <= ranNum)
+                {
+                    Console.WriteLine("the number im thinking of is higher");
+                }
+                guessCounter++;
+                Console.WriteLine("guess again");
+
+                input = Console.ReadLine();
+                intTemp = Convert.ToInt32(input);
+
+            }
+
+            Console.WriteLine($"nice job :) it took you {guessCounter} guesses. Would you like to play again? y/n ");
+            var playResponse = Console.ReadLine();
+
+            if (playResponse == "y")
+            {
+                Console.WriteLine("okay <3 yay :)");
+            }
+            if (playResponse == "n")
+            {
+                Console.WriteLine("what the fuck");
+                playAgain = false;
+            }
+        }
+        
+
+
+       
     }
 }
