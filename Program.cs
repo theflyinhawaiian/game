6cdf17073bbe3c924c11e06becac2bcf7464d644
Puffal <fireandfinn@gmail.com>
2025-07-26 18:44:33 -0400

    Generate a random word from a list of words

--- Program.cs
+++ Program.cs
@@ -3,9 +3,43 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter a number:");
-        var input = Console.ReadLine();
+        string[] words = new[]
+        {
+            "the","of","and","a","to","in","is","you","that","it",
+            "he","for","was","on","are","as","with","his","they","at",
+            "be","this","from","I","have","or","by","one","had","not",
+            "but","what","all","were","when","we","there","can","an","your",
+            "which","their","said","if","do","will","each","about","how","up",
+            "out","them","then","she","many","some","so","these","would","other",
+            "into","has","more","her","two","like","him","see","time","could",
+            "no","make","than","first","been","its","who","now","people","my",
+            "made","over","did","down","only","way","find","use","may","water",
+            "long","little","very","after","words","called","just","where","most","know",
+            "get","through","back","much","go","good","new","write","our","me",
+            "man","too","any","day","same","right","look","think","also","around",
+            "another","came","come","work","three","must","because","does","part","even",
+            "place","well","such","here","take","why","help","put","different","off",
+            "again","old","great","tell","men","say","small","every","found","still",
+            "between","home","big","give","air","line","set","own","under","read",
+            "last","never","us","left","end","along","while","might","next","sound",
+            "below","saw","something","thought","both","few","those","always","show","large",
+            "often","together","asked","house","going","want","school","important","until","form", "balls","cum"
+            
+        };
 
-        Console.WriteLine($"Your number is: {input}");
+        var random = new Random();
+        var ranWordNum = random.Next(words.Length) + 1;
+        var ranWord = words[ranWordNum];
+
+        Console.WriteLine(ranWord);
+
+
+
+
+
+
+
+
+
     }
 }
