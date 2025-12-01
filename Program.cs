d166659656b6f4fd96e1b50c625f5f287543dbc0
Puffal <fireandfinn@gmail.com>
2025-12-01 16:49:20 -0500

    Deal 2 cards to player.

--- Program.cs
+++ Program.cs
@@ -6,57 +6,76 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
-        var random = new Random();
-        var ranNum = random.Next(52) + 1;
-
-        Console.WriteLine(ranNum);
-
+        int[] playerCards = new int[52];
+        var cardsDealt = 0;
         
 
-        var rankNum = ranNum%13;
-        var rank = "";
-
-        switch (rankNum)
+        while (cardsDealt < 2)
         {
-            case 1:
-                rank = "Ace";
-                break;
-            case 11:
-                rank = "Jack";
-                break;
-            case 12:
-                rank = "Queen";
-                break;
-            case 13:
-                rank = "King";
-                break;
-            default:
-                rank = rankNum.ToString();
-                break;
+            var random = new Random();
+            var ranNum = random.Next(52);
+
+            var duplicateCard = false;
+
+            for (int j = 0; j < playerCards.Length; j++)
+            {
+                if (playerCards[j] == ranNum)
+                {
+                    duplicateCard = true;
+                }
+            }
+
+            if (duplicateCard) { continue; }
+
+            var rankNum = (ranNum % 13) + 1;
+            var rank = "";
+
+            switch (rankNum)
+            {
+                case 1:
+                    rank = "Ace";
+                    break;
+                case 11:
+                    rank = "Jack";
+                    break;
+                case 12:
+                    rank = "Queen";
+                    break;
+                case 13:
+                    rank = "King";
+                    break;
+                default:
+                    rank = rankNum.ToString();
+                    break;
+            }
+
+            var suitNum = ranNum / 13;
+            var suit = "";
+
+            switch (suitNum)
+            {
+                case 0:
+                    suit = "Spades";
+                    break;
+                case 1:
+                    suit = "Clubs";
+                    break;
+                case 2:
+                    suit = "Hearts";
+                    break;
+                case 3:
+                    suit = "Diamonds";
+                    break;
+            }
+
+            playerCards[cardsDealt] = ranNum;
+            cardsDealt++;
+
+            var cardDisplay = rank + " of " + suit;
+            Console.WriteLine(cardDisplay);
+
         }
-
-        var suitNum = ranNum / 13;
-        var suit = "";
-
-        switch (suitNum)
-        {
-            case 0:
-                suit = "Spades";
-                break;
-            case 1:
-                suit = "Clubs";
-                break;
-            case 2:
-                suit = "Hearts";
-                break;
-            case 3:
-                suit = "Diamonds";
-                break;
-        }
-
-        var card = rank + " of " + suit;
-        Console.WriteLine(card);
+        
 
     }
 }
