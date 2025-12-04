98e6f0bfbc725fe1675cf9799242a1ef547b786f
Puffal <fireandfinn@gmail.com>
2025-12-03 19:45:46 -0500

    refactored all code into functions

--- Program.cs
+++ Program.cs
@@ -4,22 +4,79 @@ using System.Runtime.InteropServices;
 
 public class Program
 {
-    public static void Main(string[] args)
+    static void PrintCard(int cardID)
     {
-        int[] playerCards = new int[52];
-        var cardsDealt = 0;
-        
+        var rankNum = (cardID % 13) + 1;
+        var rank = "";
 
-        while (cardsDealt < 2)
+        switch (rankNum)
+        {
+            case 1:
+                rank = "Ace";
+                break;
+            case 11:
+                rank = "Jack";
+                break;
+            case 12:
+                rank = "Queen";
+                break;
+            case 13:
+                rank = "King";
+                break;
+            default:
+                rank = rankNum.ToString();
+                break;
+        }
+
+        var suitNum = cardID / 13;
+        var suit = "";
+
+        switch (suitNum)
+        {
+            case 0:
+                suit = "Spades";
+                break;
+            case 1:
+                suit = "Clubs";
+                break;
+            case 2:
+                suit = "Hearts";
+                break;
+            case 3:
+                suit = "Diamonds";
+                break;
+        }
+
+        var cardDisplay = rank + " of " + suit;
+        Console.WriteLine(cardDisplay);
+    }
+
+    static void PrintHand(int[] hand, int cardsDealt)
+    {
+        for(int i = 0; i < cardsDealt; i++)
+        {
+            PrintCard(hand[i]);
+
+        }
+        
+    }
+
+    static int[] DealHand(int cardsToDeal)
+    {
+        int[] hand = new int[52];
+        var cardsDealt = 0;
+
+
+        while (cardsDealt < cardsToDeal)
         {
             var random = new Random();
-            var ranNum = random.Next(52);
+            var cardID = random.Next(52);
 
             var duplicateCard = false;
 
-            for (int j = 0; j < playerCards.Length; j++)
+            for (int j = 0; j < hand.Length; j++)
             {
-                if (playerCards[j] == ranNum)
+                if (hand[j] == cardID)
                 {
                     duplicateCard = true;
                 }
@@ -27,55 +84,20 @@ public class Program
 
             if (duplicateCard) { continue; }
 
-            var rankNum = (ranNum % 13) + 1;
-            var rank = "";
-
-            switch (rankNum)
-            {
-                case 1:
-                    rank = "Ace";
-                    break;
-                case 11:
-                    rank = "Jack";
-                    break;
-                case 12:
-                    rank = "Queen";
-                    break;
-                case 13:
-                    rank = "King";
-                    break;
-                default:
-                    rank = rankNum.ToString();
-                    break;
-            }
-
-            var suitNum = ranNum / 13;
-            var suit = "";
-
-            switch (suitNum)
-            {
-                case 0:
-                    suit = "Spades";
-                    break;
-                case 1:
-                    suit = "Clubs";
-                    break;
-                case 2:
-                    suit = "Hearts";
-                    break;
-                case 3:
-                    suit = "Diamonds";
-                    break;
-            }
-
-            playerCards[cardsDealt] = ranNum;
+            hand[cardsDealt] = cardID;
             cardsDealt++;
 
-            var cardDisplay = rank + " of " + suit;
-            Console.WriteLine(cardDisplay);
+            
 
         }
-        
+
+        return hand;
+    }
+
+    public static void Main(string[] args)
+    {
+        var playerHand = DealHand(30);
+        PrintHand(playerHand, 30);
 
     }
 }
