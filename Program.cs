ca23e3299f7f772360cc5417862921ce7b64d7ea
Puffal <fireandfinn@gmail.com>
2026-02-16 16:16:56 -0500

    bank account is made

--- Program.cs
+++ Program.cs
@@ -1,11 +1,64 @@
 ﻿
+using System;
+
+public class BankAccount
+{
+    private int Money { get; set; }
+
+    public BankAccount()
+    {
+        Money = 0;
+    }
+
+    public void DepositMoney(int depositedmoney)
+    {
+        if (depositedmoney <= 0)
+        {
+            Console.WriteLine("Please try again later with a valid number");
+            return;
+        }
+
+        Money += depositedmoney;
+        Console.WriteLine(Money);
+    }
+
+    public void DepositMoney(string depositedMoney)
+    {
+        var converted = Int32.Parse(depositedMoney);
+        DepositMoney(converted);
+    }
+
+    public void WithdrawMoney(int withdrawnMoney)
+    {
+        if (withdrawnMoney > Money)
+        {
+            Console.WriteLine("error, you cannot withdraw this much money. Your current balance is " + Money);
+            return;
+        }
+        if (withdrawnMoney <= 0)
+        {
+            Console.WriteLine("Please try again later with a valid number");
+            return;
+        }
+
+
+        Money -= withdrawnMoney;
+        Console.WriteLine(Money);
+    }
+
+
+
+
+
+}
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter a number:");
-        var input = Console.ReadLine();
+        var myBankAccount = new BankAccount();
+        myBankAccount.DepositMoney("50");
+        myBankAccount.WithdrawMoney(5);
 
-        Console.WriteLine($"Your number is: {input}");
+
     }
 }
