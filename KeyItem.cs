3dbe000bdb0b478b7580926098a2a1a148e67c81
Puffal <fireandfinn@gmail.com>
2026-02-24 15:36:30 -0500

    KeyItem stub

---
+++ KeyItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+public enum KeyValue
+{
+	Safe,
+	Gate
+}
+
+
+
+public class KeyItem : Item
+{
+	private KeyValue KeyValue;
+
+	public KeyItem(KeyValue keyValue, string itemName)
+	{
+		KeyValue = keyValue;
+		ItemName = itemName;
+	}
+}
+
+
+
