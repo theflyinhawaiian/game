317e770b209773ad85ccbd9ad6eb4d24ea3e3b3c
Puffal <fireandfinn@gmail.com>
2026-02-24 15:31:22 -0500

    Created new class Enemy

---
+++ Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public class Enemy
+{
+	private int MaxHealth { get; set; }
+	private int CurrentHealth { get; set; }
+	private int AttackStat { get; set; }
+	private string EnemyName { get; set; }
+
+	Item item { get; set; }
+
+
+
+	public Enemy(int maxHealth, int attack, Item itemOnPerson, string enemyName)
+	{
+		MaxHealth = maxHealth;
+		CurrentHealth = maxHealth;
+		AttackStat = attack;
+		EnemyName = enemyName;
+		item = itemOnPerson;
+	}
+}
