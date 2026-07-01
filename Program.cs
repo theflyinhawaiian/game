50df6a055c005e59c9091af9e376e79bc1c2a082
PuffleOboy <67072788+PuffleOboy@users.noreply.github.com>
2026-06-30 20:35:17 -0400

    wip Gen1Mech Item

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Net.Http.Headers;
+
 public class Program
 {
     public static void Main(string[] args)
@@ -10,17 +12,23 @@ public class Program
         var attackingDamageInt = (int) MathF.Round(attackingDamage);
         hero.CurrentHP -= attackingDamageInt;
 
-        Console.WriteLine(hero.CurrentHP.ToString());
+        Console.WriteLine(hero.EffectiveSpeed.ToString());
+
+        var currentUnit = princess;
 
         var damageCore = new Item("DamageCore", new List<ItemEffect> { new ItemEffect(1.1f, OperatorHandler.Multiply, Stat.DamageModifier), new ItemEffect(3f, OperatorHandler.Add, Stat.Speed) });
         var sacsPizza = new Item("SacsPizza", new List<ItemEffect> { new ItemEffect(3f, OperatorHandler.Add, Stat.Speed) });
         var sniperScope = new Item("SniperScope", new List<ItemEffect> { new ItemEffect(.1f, OperatorHandler.Add, Stat.CritChance)});
+        var coolItem = new Item("CoolItem", new List<ItemEffect> { new ItemEffect(3f, OperatorHandler.Add, Stat.Movement), new ItemEffect(1f, OperatorHandler.Add, Stat.Speed) });
+        var gen1Mech = new Item("Gen1Mech", new List<ItemEffect> { new ItemEffect((currentUnit.EffectiveSpeed / 100), OperatorHandler.Add, Stat.CritChance) });
 
         var list = new List<Item> {};
 
         list.Add(damageCore);
         list.Add(sacsPizza);
         list.Add(sniperScope);
+        list.Add(coolItem);
+        list.Add(gen1Mech);
 
         princess.Inventory = new Inventory(list, princess);
 
@@ -33,6 +41,7 @@ public class Program
 
         Console.WriteLine(princess.EffectiveDamage.ToString());
         Console.WriteLine(princess.EffectiveDamageModifier.ToString());
+        Console.WriteLine(princess.EffectiveCritChance.ToString());
 
         attackingDamage = princess.EffectiveDamage * princess.EffectiveDamageModifier;
         attackingDamageInt = (int)MathF.Round(attackingDamage);
