5927d6a4b179558053c37664a7a43a9c4f056151
PuffleOboy <67072788+PuffleOboy@users.noreply.github.com>
2026-08-12 14:57:54 -0400

    wip collection initializer system for items

--- ItemEffect.cs
+++ ItemEffect.cs
@@ -4,16 +4,16 @@ public class ItemEffect
 {
     public float ModificationNumber { get; set; }
     public OperatorHandler OperatorSign { get; set; }
-    public Stat StatModified { get; set; }
+    public Stat StatModified { get; set; } 
     public TriggerType TriggerType { get; set; }
-    public Func<TriggerContext, bool> TriggerCondition { get; set; }
+    public Func<TriggerContext, bool> TriggerCondition { get; set; } = ctx => true;
+    public IStatus? Status { get; set; } = null;
 
-    public ItemEffect(float modificationNumber, OperatorHandler operatorSign, Stat statModified, TriggerType triggerType, Func<TriggerContext, bool> triggerCondition)
+    public ItemEffect(float modificationNumber, OperatorHandler operatorSign, Stat statModified, TriggerType triggerType)
 	{
         ModificationNumber = modificationNumber;
         OperatorSign = operatorSign;
         StatModified = statModified;
         TriggerType = triggerType;
-        TriggerCondition = triggerCondition;
 	}
 }
